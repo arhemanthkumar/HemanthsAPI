32b96fbc7cacd2e0cb9423f1e1948acd6ddc7482
Hemanth Kumar <hemanth@Hemanths-MacBook-Air.local>
2026-03-03 01:53:05 +0530

    Initial learnings of FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+# Importing the FastAPI module
 from fastapi import FastAPI
 
+# We are creating an (object - app) instance of a class called FastAPI
+# This will be the main point of interaction to create all the APIs
 app = FastAPI()
 
+
+# This is called as a path operation
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(): # async needed only when time constraints are present in function calling, can remove it.
+    return {"message": "Hello World"} # Here FastAPI automatically converts python dictionaries into JSON
+
+'''
+To summarize this:
+get -> Method name
+"/" -> Path name
+root() -> Function name
+'''
