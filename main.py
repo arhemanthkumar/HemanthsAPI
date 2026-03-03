61c691341df40b4a6e71d34b5b02a7f1ca89dbcf
Hemanth Kumar <hemanth@Hemanths-MacBook-Air.local>
2026-03-03 23:41:09 +0530

    Input Validations

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from fastapi import FastAPI
 
 from fastapi.params import Body
 
+from pydantic import BaseModel # For input schema validation
+# Pydantic has lot of inbuilt data types which we can use to validate
+
 # We are creating an (object - app) instance of a class called FastAPI
 # This will be the main point of interaction to create all the APIs
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True # Keeping True as default
 
 # This is called as a path operation
 @app.get("/") # A "path" is also commonly called an "endpoint" or a "route".
@@ -25,7 +32,12 @@ root() -> Function name
 def get_posts():
     return {"data": "This is your posts"}
 
+# @app.post("/createposts") # Creating a POST method witht the path name -> /createposts
+# def create_posts(payLoad: dict = Body(...)): # This captures the Body content passed in JSON in dictionary format in the payLoad variable
+#     print(payLoad) # Printing to check the contents
+#     return {"Success": f"Your Title: {payLoad['title']} and Your Content: {payLoad['content']}"} # Return message
+
 @app.post("/createposts") # Creating a POST method witht the path name -> /createposts
-def create_posts(payLoad: dict = Body(...)): # This captures the Body content passed in JSON in dictionary format in the payLoad variable
-    print(payLoad) # Printing to check the contents
-    return {"messsage": "Successfully created posts"} # Return message
+def create_posts(posts: Post): # Here we are doing input validation by checking if the variable posts has the title and content and are of right type by using Post Extended class
+    print(posts) # Printing to check the contents
+    return {"Success"} # Return message
