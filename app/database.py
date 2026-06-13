4ef027a1c20bb87b31b9802b3813bf98ef37e0c5
arhemanthkumar <hemanthar95@gmail.com>
2026-06-13 19:53:19 +0530

    Created sqlAlchemy session -> Database_URL, Engine, SessionLocal

--- app/database.py
+++ app/database.py
@@ -1,25 +1,24 @@
-from typing import Annotated
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-
-from sqlalchemy.orm import DeclarativeBase
-
-# SQLALCHEMY_DATABASE_URL = "postgresql:<username>:<password>@<ip-address/hostname>/<database_name>"
-SQLALCHEMY_DATABASE_URL = "postgresql+pyscopg:postgres:20032003@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg://postgres:20032003@localhost/fastapi"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-sessionLocal = Session(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 class Base(DeclarativeBase):
     pass
-
-# Fom FastAPI SQLAlchemy docs.
-# class Hero(SQLModel, table=True):
-#     id: int | None = Field(default=None, primary_key=True)
-#     name: str = Field(index=True)
-#     age: int | None = Field(default=None, index=True)
-#     secret_name: str
-
