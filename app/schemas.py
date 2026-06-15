b60e1c4de54c69a1f2c81978df41c088c64d6ddd
arhemanthkumar <hemanthar95@gmail.com>
2026-06-15 09:01:55 +0530

    Created proper classes for post responses validation which extends base post class

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict # For input schema validation
+# Pydantic has lot of inbuilt data types which we can use to validate
+# https://pydantic.dev/docs/validation/1.10/usage/types/
+
+from datetime import datetime
+
 
 # SCHEMA from Pydantic Model
 class Post(BaseModel):
     title: str
     content: str
-    published: bool = True # Keeping True as default value
+    published: bool = True # Keeping True as default 
+    
+
+class PostCreate(Post):
+    pass
+
+class PostResponse(Post): # For validation of responses which are sent back to the user.
+    # title: str
+    # content: str
+    # published: bool # These can be avoided as it is inheriting the Post Class which is Base Model
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
